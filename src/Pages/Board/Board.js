e0da249404080e9215504cc630b0ee8990da8bab
BHammock33 <BennettHammock@gmail.com>
2023-07-09 15:48:19 -0400

    Project manager

---
+++ src/Pages/Board/Board.js
@@ -0,0 +1,45 @@
+import Lane from '../../Components/Lane';
+import './Board.css';
+import { useState, useEffect } from 'react';
+
+const lanes = [
+    { id: 1, title: 'To Do' },
+    { id: 2, title: 'In Progress' },
+    { id: 3, title: 'Review' },
+    { id: 4, title: 'Done' },
+];
+
+
+function Board(){
+const [loading, setLoading] = useState(false);
+const [tasks, setTasks] = useState([]);
+const [error, setError] = useState("");
+useEffect(() =>{
+    async function fetchData(){
+        try{
+            const tasks = await fetch(
+                `https://my-json-server.typicode.com/PacktPublishing/React-Projects-Second-
+                Edition/tasks`,
+            );
+            const res = await tasks.json();
+            if(res){
+                setTasks(res);
+                setLoading(false);
+            }
+        }catch(e){
+            setLoading(false);
+            setError(e.message);
+        }
+    }
+    fetchData();
+},[]);
+    return(
+        <div className='Board-wrapper'>
+            {lanes.map((lane) => (
+                <Lane key={lane.id} title={lane.title} loading ={loading} error = {error} tasks ={tasks.filter((task) => tasks.lane === lane.id)} />
+            ))}
+        </div>
+    );
+}
+
+export default Board;
